d38c6969ece5368c9e6dacd1c1f1d703543d1cdd
Everson <140653717+Eversonnnn@users.noreply.github.com>
2025-05-17 16:09:28 -0300

    Criando as Cartas do Super Trunfo
    Desafio: nível novato

---
+++ super_trunfo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    // Dados para Carta 1
+    char estado1 = 'A';
+    char codigo1[] = "A01";
+    char nome1[] = "São Paulo";
+    int populacao1 = 12325000;
+    float area1 = 1521.11;
+    float pib1 = 699.28;
+    int pontos_turisticos1 = 50;
+    
+    // Dados para Carta 2
+    char estado2 = 'B';
+    char codigo2[] = "B02";
+    char nome2[] = "Rio de Janeiro";
+    int populacao2 = 6748000;
+    float area2 = 1200.25;
+    float pib2 = 300.50;
+    int pontos_turisticos2 = 30;
+    
+    // Exibição dos dados das cartas
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nome1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nome2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    
+    return 0;
+}
